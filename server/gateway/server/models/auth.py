712489ad1075bf59d0fd6c13d99fb3c639a3a3ea
tamir <tamir3837@gmail.com>
2024-06-30 21:39:02 +0300

    feat(login for gateway)

---
+++ server/gateway/server/models/auth.py
@@ -0,0 +1,19 @@
+from datetime import timedelta
+
+from pydantic import BaseModel
+
+from  jwt import ACCESS_EXPIRES, REFRESH_EXPIRES
+
+
+class AccessToken(BaseModel):
+    """Access token details."""
+
+    access_token: str
+    access_token_expires: timedelta = ACCESS_EXPIRES
+
+
+class RefreshToken(AccessToken):
+    """Access and refresh token details."""
+
+    refresh_token: str
+    refresh_token_expires: timedelta = REFRESH_EXPIRES
